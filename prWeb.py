8a0fb7f0a8690f15f3f8440c21b4a30090de516e
emad-mohamadi <seyedemadmohammadi@gmail.com>
2025-09-18 02:21:08 +0330

    Update prWeb.py

--- prWeb.py
+++ prWeb.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('https://drive.google.com/uc?id=1RigQagTT8JfNTCLhP461iTaCs7fwBVYv', encoding='latin1', low_memory=False)
+df = pd.read_excel('https://docs.google.com/spreadsheets/d/19xdrrRJURV4-Ts_GMUUC8sSe2T5pAqWe/export?format=xlsx', engine='openpyxl')
 
 st.title("Professor Search App")
 
@@ -77,3 +77,4 @@ if st.checkbox("Do you want to see a histogram of a numeric column?"):
     ax.set_ylabel("Frequency")
 
     st.pyplot(fig)
+
